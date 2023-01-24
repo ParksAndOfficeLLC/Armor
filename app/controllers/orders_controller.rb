5ae2aa0ae7666f3c5759b711560a831836b12457
Andre Jordan Gahan <107956416+ajgahan19@users.noreply.github.com>
2023-01-23 16:29:08 -0800

    created-create-methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,18 @@ class OrdersController < ApplicationController
         orders = Order.all 
         render json: orders
     end
+    def create
+        order = order.create(order_params)
+        if order.valid?
+        render json: order
+        else render json: order.errors, status:422
+    end  
+    end
+    private 
+    def orders_params
+        params.require(:order).permit(:user_id, :product_id)
+    end
 end
+
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_and_belongs_to_many :orders
-  has_and_belongs_to_many :products
+  # has_and_belongs_to_many :products
+  has_many :products, through: :orders
 end
