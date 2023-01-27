759f279b853aaa85ed49c52c6a24e3fda5ba91b4
MikeSC27 <mcruzen89@gmail.com>
2023-01-27 12:02:55 -0800

    create fetch and jest passing

---
+++ app/javascript/components/pages/OrderNew.test.js
@@ -0,0 +1,28 @@
+/**
+* @jest-environment jsdom
+*/
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import '@testing-library/jest-dom'
+import OrderNew  from "./OrderNew"
+import { BrowserRouter, MemoryRouter } from "react-router-dom"
+
+describe("<OrderNew />", () => {
+  beforeEach(() => {
+    render(
+    // <MemoryRouter>
+      <BrowserRouter>
+        <OrderNew />
+      </BrowserRouter>
+    // </MemoryRouter>
+    )
+  })
+  it("renders without crashing", () => {
+    expect(screen.getByText("Please sign in to add new orders")).toBeInTheDocument 
+})
+
+//   it("has a form with entries for product_id", () => {
+//     const formProduct_id = screen.getByText("Product ID");
+//     expect(formProduct_id.getAttribute("For")).toEqual("Product ID");
+//   })
+})
