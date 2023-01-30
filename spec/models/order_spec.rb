6c456fca0723f92742871b4a31feaefa6ac31053
MikeSC27 <mcruzen89@gmail.com>
2023-01-30 14:45:03 -0800

    validations

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  # Validations
+
+  let(:user) { User.create }
+  let(:product) { Product.create }
+
+  it "validates an order has a user_id" do
+    order = Order.new(user_id: user.id, product_id: product.id)
+    expect(order.errors).to be_empty
+  end
+
+  it "validates an order has a product_id" do
+    order = Order.new(user_id: user.id, product_id: product.id)
+    expect(order.errors).to be_empty
+  end
+  it "validates a product has a created at datetime" do
+    order = Order.new
+    expect(order.errors).to be_empty
 end
+  it "validates a product has a updated at datetime" do
+    order = Order.new
+    expect(order.errors).to be_empty
+end
+end
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    # Validations
-  pending "add some examples to (or delete) #{__FILE__}"
+  # it "validates a user has a first name" do
+  #     # user = User.create
+  #     expect(users.errors).to be_empty
+  # end
+  # it "validates a user has a last name" do
+  #     # user = User.create
+  #     expect(users.errors).to be_empty
+  # end
+  # it "validates a user has a email" do
+  #     # user = User.create
+  #     expect(users.errors).to be_empty
+  # end
+  # it "validates a user has a password" do
+  #     # user = User.create
+  #     expect(users.errors).to be_empty
+  # end
 end
